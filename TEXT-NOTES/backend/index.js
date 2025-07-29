913e7f8262fec9034e209f623b2fbef1373ec6db
iamanzalshaikh <tocxanzalftw49@gmail.com>
2025-07-29 16:34:13 +0530

    Update index.js

--- TEXT-NOTES/backend/index.js
+++ TEXT-NOTES/backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: ["http://localhost:5173", "https://text-notes.onrender.com"],
+    origin: ["http://localhost:5173", "https://notes-frontendtext-notes-frontend-my.onrender.com"],
     credentials: true,
 }));
 
